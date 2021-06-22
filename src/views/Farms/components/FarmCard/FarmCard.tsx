c9564504176c35d23c55903a34a6936ce7563682
AyaSwap <86141106+AyaSwap@users.noreply.github.com>
2021-06-22 11:40:37 +0700

    Update FarmCard.tsx
    Update polygonscan

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -162,8 +162,8 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, account }
       <ExpandingWrapper expanded={showExpandableSection}>
         <DetailsSection
           removed={removed}
-          bscScanAddress={`https://bscscan.com/address/${farm.lpAddresses[process.env.REACT_APP_CHAIN_ID]}`}
-          infoAddress={`https://pancakeswap.info/pair/${lpAddress}`}
+          bscScanAddress={`https://polygonscan.com/address/${farm.lpAddresses[process.env.REACT_APP_CHAIN_ID]}`}
+          infoAddress={`https://info.quickswap.exchange/pair/${lpAddress}`}
           totalValueFormatted={totalValueFormatted}
           lpLabel={lpLabel}
           addLiquidityUrl={addLiquidityUrl}
