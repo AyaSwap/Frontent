979d59ac7fa5ffd701980c0134d1204ac5dfb0bf
AyaSwap <86141106+AyaSwap@users.noreply.github.com>
2021-06-25 22:10:57 +0700

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -16,6 +16,20 @@ const pools: PoolConfig[] = [
     sortOrder: 1,
     isFinished: false,
   },
+  {
+    sousId: 100,
+    stakingToken: tokens.wmatic,
+    earningToken: tokens.deko,
+    contractAddress: {
+      97: '',
+      137: '0xe2fcfadc9502f6fe7f2188fe5320edf8f5ab2e2e',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '0.1',
+    sortOrder: 2,
+    isFinished: false,
+  },
   
 ]
 
